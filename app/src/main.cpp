e82e1b7e7949a85b9d74849926d10605c5758af1
Yauhen Matskevich <evgeniy.m@iomico.com>
2026-04-11 16:01:19 +0200

    Design a Hierarchical Menu

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,8 +2,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS 1000
-
 /* The devicetree node identifier for the "led0" alias. */
 #define LED_NODE DT_ALIAS(led0)
 
@@ -24,7 +22,7 @@ int main(void)
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
+        k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
     }
     return 0;
 }
