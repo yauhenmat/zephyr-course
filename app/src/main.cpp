e2ce8d7670ae6c218b763448d2a4115e9d28b534
Yauhen Matskevich <evgeniy.m@iomico.com>
2026-04-12 21:41:01 +0200

    add external led with dts overlay

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,8 +4,10 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED_NODE DT_ALIAS(led0)
+#define LED_NODE_1 DT_ALIAS(led1)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED_NODE_1, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
@@ -14,11 +16,14 @@ int main(void)
     bool led_state = true;
 
     if (!gpio_is_ready_dt(&led)) return 0;
+    if (!gpio_is_ready_dt(&led1)) return 0;
 
     if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE) < 0) return 0;
 
     while (1) {
         if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        if (gpio_pin_toggle_dt(&led1) < 0) return 0;
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
